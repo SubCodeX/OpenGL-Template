6f243a4c0aee039acda1ba37fc3738756ae91f5c
Mattias Holm <subcodex@gmail.com>
2021-11-24 17:27:44 +0100

    Add project files.

---
+++ OpenGL Template/OpenGL Template.cpp
@@ -0,0 +1,12 @@
+﻿// OpenGL Template.cpp : Defines the entry point for the application.
+//
+
+#include "OpenGL Template.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
